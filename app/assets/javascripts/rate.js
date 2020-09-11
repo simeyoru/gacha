3dcfddbe72969da7e40ef27cd315abe3555bcbbb
simeyoru <kemokumokemo@gmail.com>
2020-09-11 15:24:02 +0900

    commit

---
+++ app/assets/javascripts/rate.js
@@ -0,0 +1,7 @@
+var rate = function () {
+  //alert("SSR:3%\nSR:15%\nR:82%");
+  $('.gacha_main__rate').append("<br>\n"); 
+  $('.gacha_main__rate').append("SSR:3%"+"<br>\n"); 
+  $('.gacha_main__rate').append("SR:15%"+"<br>\n"); 
+  $('.gacha_main__rate').append("R:82%"+"<br>\n"); 
+}
